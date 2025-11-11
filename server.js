12567af107643415ba0acd6fe85638d38b1fd2dc
diyayat <diya@yatindia.com>
2025-11-11 13:54:06 +0530

    cors for everyone allowed

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true,
+  origin: '*', // Allow all origins
+  credentials: false, // Must be false when origin is '*'
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
